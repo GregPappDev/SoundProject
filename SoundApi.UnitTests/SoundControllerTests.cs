39fdecfb6392572d33f5cd9ad284f6684d295fe0
GregPappDev <gregpappdev@gmail.com>
2023-12-07 17:46:35 +0100

    add unit test for isCreateSoundValid method with invalid data

--- SoundApi.UnitTests/SoundControllerTests.cs
+++ SoundApi.UnitTests/SoundControllerTests.cs
@@ -1,18 +1,23 @@
 using Contract;
 using FlatBuffers;
 using Microsoft.AspNetCore.Authorization;
+using NSubstitute;
 using SoundApi.Controllers;
+using SoundApi.Data;
 using SoundApi.Interfaces;
 
 namespace SoundApi.UnitTests
 {
     public class SoundControllerTests
     {
-        SoundController _controller;
+        private readonly SoundController _sut;
+        //private readonly AppDbContext _context = Substitute.For<AppDbContext>();
+        private readonly ISoundService _service = Substitute.For<ISoundService>();
 
-        public SoundControllerTests(SoundController controller)
+        public SoundControllerTests()
         {
-            _controller = controller;
+
+            _sut = new SoundController(_service);
         }
 
         [Fact]
@@ -33,11 +38,37 @@ namespace SoundApi.UnitTests
 
             // Act
 
-            bool result = _controller.isCreateSoundValid(soundRoot);
+            bool result = _sut.isCreateSoundValid(soundRoot);
 
             // Assert
 
             Assert.True(result);
+
+        }
+
+        [Fact]
+        public void IsCreateSoundValid_ReturnsFalse_IfInputTypeIsInvalid()
+        {
+            // Arrange
+            var builder = new FlatBufferBuilder(1);
+            var name = builder.CreateString("");
+            var extension = builder.CreateString("");
+            var sound = CreateSound.CreateCreateSound(builder, name, extension);
+            CreateSound.StartCreateSound(builder);
+            CreateSound.AddName(builder, name);
+            CreateSound.AddExtension(builder, extension);
+            var builtSound = CreateSound.EndCreateSound(builder);
+            builder.Finish(builtSound.Value);
+            var buf = builder.DataBuffer;
+            var soundRoot = CreateSound.GetRootAsCreateSound(buf);
+
+            // Act
+
+            bool result = _sut.isCreateSoundValid(soundRoot);
+
+            // Assert
+
+            Assert.False(result);
 
         }
     }
