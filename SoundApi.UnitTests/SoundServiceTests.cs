205927c38daf7d3efa4aad35fcb4b8b8c91a4d6b
GregPappDev <gregpappdev@gmail.com>
2023-12-07 21:22:06 +0100

    add unit tests for both outcomes of CreateStreamFromCreateSoundType method

--- SoundApi.UnitTests/SoundServiceTests.cs
+++ SoundApi.UnitTests/SoundServiceTests.cs
@@ -25,7 +25,7 @@ namespace SoundApi.UnitTests
         {
             // Arrange
             var validCreateSound = CreateSoundType("Enter Sandman", "mp3");
-
+            
             // Act
             bool result = _sut.IsCreateSoundValid(validCreateSound);
 
@@ -46,6 +46,36 @@ namespace SoundApi.UnitTests
             Assert.False(result);
         }
 
+        [Fact]
+        public void ConvertStreamToCreateSoundType_ReturnsTrue_IfInputTypeIsValid()
+        {
+            // Arrange
+            Stream stream = CreateStreamFromCreateSoundType("Enter Sandman", "mp3");
+            
+            // Act
+            var result = _sut.ConvertStreamToCreateSoundType(stream);
+
+            // Assert
+            
+            Assert.True(result.GetType () == typeof(CreateSound));
+
+        }
+
+        [Fact]
+        public void ConvertStreamToCreateSoundType_ReturnsFalse_IfInputTypeInvalid()
+        {
+            // Arrange
+            var emptyByteArray = new byte[0];
+            Stream stream = new MemoryStream(emptyByteArray);
+
+            // Act
+            var result = _sut.ConvertStreamToCreateSoundType(stream);
+
+            // Assert
+            Assert.Null(result);
+
+        }
+
 
         #region Helper functions
 
@@ -54,15 +84,43 @@ namespace SoundApi.UnitTests
             var builder = new FlatBufferBuilder(1);
             var name = builder.CreateString(soundName);
             var extension = builder.CreateString(soundExtention);
+
             var sound = CreateSound.CreateCreateSound(builder, name, extension);
+
             CreateSound.StartCreateSound(builder);
             CreateSound.AddName(builder, name);
             CreateSound.AddExtension(builder, extension);
             var builtSound = CreateSound.EndCreateSound(builder);
+
             builder.Finish(builtSound.Value);
+
             var buf = builder.DataBuffer;
             var soundRoot = CreateSound.GetRootAsCreateSound(buf);
+          
             return soundRoot;
+        }
+
+        private Stream CreateStreamFromCreateSoundType(string soundName, string soundExtention)
+        {
+            var builder = new FlatBufferBuilder(1);
+            var name = builder.CreateString(soundName);
+            var extension = builder.CreateString(soundExtention);
+
+            var sound = CreateSound.CreateCreateSound(builder, name, extension);
+
+            CreateSound.StartCreateSound(builder);
+            CreateSound.AddName(builder, name);
+            CreateSound.AddExtension(builder, extension);
+            var builtSound = CreateSound.EndCreateSound(builder);
+
+            builder.Finish(builtSound.Value);
+
+            var buf = builder.DataBuffer;
+            var soundRoot = CreateSound.GetRootAsCreateSound(buf);
+                        
+            byte[] byteArray = builder.SizedByteArray();
+            Stream stream = new MemoryStream(byteArray);
+            return stream;
         }
 
         #endregion
